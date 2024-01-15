d0cfa0f0b22389bae1ebf84e8ceff2abd57505b3
cataling6 <radoi.catalin88@gmail.com>
2024-01-15 12:26:38 +0100

    up icona carrello

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function creaCard(libri) {
   libri.map((x, indexCreazione) => {
     indexCreazione + 1;
     divContenitoreLibri.innerHTML += `<div class="card" style="width: 18rem" id="card-${indexCreazione}">
-  <div class="d-flex justify-content-between align-items-start mt-3"><img src="${x.img}" class="card-img-top" alt="..." /><p class="badge bg-secondary d-none" id="badge-${indexCreazione}">aggiunto</p></div>
+  <div class="d-flex justify-content-between align-items-start mt-3"><img src="${x.img}" class="card-img-top" alt="..." /><p class="badge bg-secondary d-none" id="badge-${indexCreazione}"><i class="bi bi-cart-check"></i> aggiunto</p></div>
   <div class="card-body d-flex flex-column justify-content-between" id="card-body-${indexCreazione}">
     <h5 class="card-title">${x.title}</h5>
     <h6 class="card-subtitle mb-2 text-muted">${x.category}</h6>
